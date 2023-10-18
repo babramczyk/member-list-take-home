4d9f9860647113c95d0cd7b4ee43767c35617ac0
Brett Abramczyk <bjabramczyk@gmail.com>
2023-10-18 10:21:58 -0500

    add very basic tabs (Radix) at top of page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Member, fetchMembers } from "./data/members";
 import "./App.css";
 import { MemberRow } from "./components/MemberRow";
+import * as Tabs from "@radix-ui/react-tabs";
 
 export default function App() {
   const [members, setMembers] = useState<Member[] | null>(null);
@@ -13,11 +14,20 @@ export default function App() {
 
   return (
     <div className="h-full p-16">
-      <div className="border-2 border-slate-900 h-full flex flex-col">
-        {members?.map((member) => (
-          <MemberRow key={member.id} member={member} />
-        ))}
-      </div>
+      <Tabs.Root defaultValue="Members">
+        <Tabs.List>
+          <Tabs.Trigger value="members">Members</Tabs.Trigger>
+          <Tabs.Trigger value="groups">Groups</Tabs.Trigger>
+        </Tabs.List>
+        <div className="border-2 border-slate-900 h-full flex flex-col">
+          <Tabs.Content value="members">
+            {/* TODO: Figure out a fancy way to not recreate elements for each row? Would be better for perf if we need that one day, + could do some fun animation stuff as rows move around */}
+            {members?.map((member) => (
+              <MemberRow key={member.id} member={member} />
+            ))}
+          </Tabs.Content>
+        </div>
+      </Tabs.Root>
     </div>
   );
 }
