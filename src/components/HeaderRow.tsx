74ef0a20154a62625f0b07dfd6718d5144dc20fd
Brett Abramczyk <bjabramczyk@gmail.com>
2023-10-19 09:22:36 -0500

    tweak header row text styling

--- src/components/HeaderRow.tsx
+++ src/components/HeaderRow.tsx
@@ -4,8 +4,8 @@ export function HeaderRow() {
   return (
     <Row className="bg-slate-100">
       {{
-        column2: "Name",
-        column3: "Admin",
+        column2: <span className="uppercase text-slate-500">Name</span>,
+        column3: <span className="uppercase text-slate-500">Admin</span>,
       }}
     </Row>
   );
